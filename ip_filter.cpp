2d365c0024b281e1e89e308e9d93f9a266e1ec56
priestalex76 <priestalex@yandex.ru>
2023-10-23 21:14:11 +0600

    Realise with lambda function, and predicat filtering

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
+#include <array>
 #include <cstdint>
 #include <cstring>
 #include <iostream>
 #include <set>
 #include <string>
 #include <vector>
-#define OCTET_COUNT 4
 
 std::vector<std::string> split(const std::string &str, char d) {
   std::vector<std::string> r;
@@ -24,62 +24,37 @@ std::vector<std::string> split(const std::string &str, char d) {
   return r;
 }
 
-// Для ревьювера. Реализовал максимально отказавшись от принципов KISS и YAGNI
-// в надежде получить побольше комментариев по применению ООП и лямбд.
-// Разумеется можно было всё cделать через обычные функции и сортировку
-// контейнера vector<vector<string>>. Можно было и с использованием ranges 20-го
-// стандарта, но надо ли?
-
 using std::string, std::vector;
-
 namespace otus {
-
 class IP {
-  vector<uint8_t> ip_{};
+
+  using container = std::array<uint8_t, 4>;
+  container ip_;
 
 public:
-  IP() = default; //Пересмотрел лекцию по инициализации добавил дефолтный конструктор
+  IP() = default;
 
-  IP(string ipstr) {
-    for (auto &octet : split(ipstr, '.')) {
-      ip_.push_back(static_cast<uint8_t>(stoi(octet)));
+  IP(const string &ipstr) {
+    int i = 0;
+    for (const auto &octet : split(ipstr, '.')) {
+      ip_.at(i) = static_cast<uint8_t>(stoi(octet));
+      ++i;
     }
   }
 
-  vector<uint8_t> get_ip() { return ip_; }
-  bool operator==(const IP &other) const { return ip_ == other.ip_; }
+  bool operator<(const IP &other) const { return ip_ < other.ip_; }
 
-  void ip_to_uint32(uint32_t &ip32, const uint8_t &ip) const {
-    ip32 = (ip32 << 8) | ip;
-  }
+  bool operator>(const IP &other) const { return ip_ > other.ip_; }
 
-  bool operator<(const IP &other) const {
+  using iterator = typename container::iterator;
+  iterator begin() { return ip_.begin(); }
+  iterator end() { return ip_.end(); }
 
-    uint32_t ipl{0}, ipr{0};
-    for (int i = 0; i < OCTET_COUNT; ++i) {
-      ip_to_uint32(ipl, ip_.at(i));
-      ip_to_uint32(ipr, other.ip_.at(i));
-    }
-    return (ipl < ipr ? true : false);
-  }
-
-  bool operator>(const IP &other) const {
-    auto ita = other.ip_.begin();
-    uint32_t ipl{0}, ipr{0};
-    for_each(ip_.begin(), ip_.end(), [this, &ipl, &ipr, &ita](auto &itb) {
-      ip_to_uint32(ipl, itb);
-      ip_to_uint32(ipr, *ita++);
-    });
-    return (ipl > ipr ? true : false);
-  }
   void print() {
     string ipstr{};
-    for_each(std::begin(ip_), std::end(ip_),
-             [&ipstr](auto &octet) { ipstr += std::to_string(octet) + "."; });
-    //Для ревьювера. Цикл здесь смотрится даже более органично:
-    /*for (auto octet : ip_) {
+    for (auto octet : ip_) {
       ipstr += std::to_string(octet) + ".";
-    }*/
+    }
     ipstr.pop_back();
     std::cout << ipstr << std::endl;
   }
@@ -88,41 +63,21 @@ public:
 };
 
 class IPPool : IP {
-  std::multiset<IP, std::greater<>> ip_pool{};
+  std::multiset<IP, std::greater<>> ip_pool;
 
 public:
-  IPPool() = default;
-
-  void insert(IP &ip) { ip_pool.insert(ip); }
+  void insert(const IP &ip) { ip_pool.insert(ip); }
 
   void print() {
     for (auto ip : ip_pool) {
       ip.print();
     }
   }
-  void print(vector<int> filter) {
+
+  template <class Predicat> void print(Predicat filter) {
     for (auto ip : ip_pool) {
-      bool is_equal = false;
-      for (int i = 0; i < static_cast<int>(filter.size()); ++i) {
-        if (+(ip.at(i)) != static_cast<uint8_t>(filter.at(i))) {
-          is_equal = false;
-          break;
-        } else {
-          is_equal = true;
-        }
-      }
-      if (is_equal)
+      if (filter(ip))
         ip.print();
-    }
-  }
-  void print(int filter) {
-    for (auto ip : ip_pool) {
-      for (int i = 0; i < OCTET_COUNT; ++i) { //Не реализовал в классе IP итераторы, поэтому так
-        if (+(ip.at(i)) == static_cast<uint8_t>(filter)) {
-          ip.print();
-          break;
-        }
-      }
     }
   }
 };
@@ -136,10 +91,33 @@ int main() {
       otus::IP ip{split(line, '\t').at(0)};
       ipp.insert(ip);
     }
-    ipp.print();             // no filtering
-    ipp.print({{1}});        // ip = filter(1) by mask
-    ipp.print({{46}, {70}}); // ip = filter(46, 70) by mask
-    ipp.print(46);           // ip = filter_any(46)
+
+    ipp.print(); // no filtering
+
+    //Для ревьюера. Честно говоря мне не нравится, как ниже выглядит код с
+    //лямбдами. нет унификации. Возможно стоит всё вынести в функциональный
+    //объект или в вариадик или в шаблонный объект принимающий в конструкторе
+    //итераторы с deduction guide? Ваше мнение?
+
+    vector<uint8_t> octets{1}; // ip = filter(1) by mask
+    auto filter = [&](otus::IP &ip) -> bool {
+      for (int i = 0; i < static_cast<int>(octets.size()); ++i)
+        if (ip.at(i) != octets.at(i))
+          return false;
+      return true;
+    };
+    ipp.print(filter); // end ip = filter(1) by mask
+
+    octets.clear(); // ip = filter(46, 70) by mask
+    octets = {46, 70};
+    ipp.print(filter);
+
+    uint8_t octet = 46; // ip = filter_any(46)
+    auto filter_any = [&](otus::IP &ip) -> bool {
+      return std::any_of(ip.begin(), ip.end(),
+                         [&](uint8_t &i) { return i == octet; });
+    };
+    ipp.print(filter_any);
 
   } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
